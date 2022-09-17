54a9b6d37dff04ff43512683c8d7dd8bd47547ea
Hasith Methmal Jayasekara <hasith.m@eyepax.com>
2022-09-18 03:30:16 +0530

    RNR2 added

--- 3-interpolate-color/App.tsx
+++ 3-interpolate-color/App.tsx
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Switch, Text, View } from "react-native";
+
+const Colors = {
+  dark: {
+    background: "#1E1E1E",
+    circle: "#252525",
+    text: "#F8F8F8",
+  },
+  light: {
+    background: "#F8F8F8",
+    circle: "#FFF",
+    text: "#1E1E1E",
+  },
+};
+
+const SWITCH_TRACK_COLOR = {
+  true: "rgba(256,0,256,0.2)",
+  false: "rgba(0,0,0,1)",
+};
+
+type Theme = "light" | "dark";
 
 export default function App() {
+  const [theme, setTheme] = useState<Theme>("light");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Switch
+        value={theme === "dark"}
+        onValueChange={(toggled) => {
+          setTheme(toggled ? "dark" : "light");
+        }}
+      />
     </View>
   );
 }
@@ -13,8 +40,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
